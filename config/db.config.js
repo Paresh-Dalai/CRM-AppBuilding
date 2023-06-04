ffeeaefe10c3b9541d850d38be6279e2e62ed489
paresh-dalai <amarjeetdalai17@gmail.com>
2023-06-04 19:39:13 +0530

    db_config_changes

--- config/db.config.js
+++ config/db.config.js
@@ -2,5 +2,5 @@
 
 module.exports = {
     DB_NAME : "crm_DB",
-    DB_URL : 'mongodb+srv://Paresh_Dalai:Paresh@123@crm-db.tvsmgwk.mongodb.net/'
+    DB_URL : 'mongodb+srv://Paresh_Dalai:Amarjeet123@crm-db.tvsmgwk.mongodb.net/'
 }
