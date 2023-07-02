a560f4e2be1787bde26ca60010ae3d71b7ef59b8
paresh-dalai <amarjeetdalai17@gmail.com>
2023-07-03 01:29:14 +0530

    admin responsibility changed

--- Routes/auth.route.js
+++ Routes/auth.route.js
@@ -9,7 +9,8 @@ const UserController = require("./../Controller/User.controller")
    app.post("/crm/api/v1/auth/signup", [Middlewares.verifySignUp], authController.Signup)
    app.post("/crm/api/v1/auth/signin", authController.SignIn)
    app.get("/crm/api/v1/users"  , UserController.getAllUsers)
-   app.get("/crm/api/v1/users/:userId" , [AuthMiddlewares.verifyToken , AuthMiddlewares.isAdmin] , UserController.getUserById)
+  //  app.get("/crm/api/v1/users/:userId" , [AuthMiddlewares.verifyToken , AuthMiddlewares.isAdmin] , UserController.getUserById)
+  app.get("/crm/api/v1/users/:userId" , [AuthMiddlewares.verifyToken] , UserController.getUserById)
    app.put("/crm/api/v1/users/:userId" , [AuthMiddlewares.verifyToken , AuthMiddlewares.isAdmin] , UserController.updateUserDetails)
 }
 
