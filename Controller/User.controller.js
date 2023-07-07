0c80dabcbe181ea1c5b45345151bbbab8575668a
paresh-dalai <amarjeetdalai17@gmail.com>
2023-07-07 22:06:42 +0530

    LL_3

--- Controller/User.controller.js
+++ Controller/User.controller.js
@@ -35,9 +35,9 @@ const getAllUsersUserType = async (req,res) => {
     try {
        
         // let OurUsers = await User.find({userType : Constants.userTypes.engineer || Constants.userTypes.customer})
-     if(enteredUserType == CUSTOMER) {
+     if(enteredUserType == "CUSTOMER") {
          OurUsers = await User.find({userType : "CUSTOMER"})
-     }else if(enteredUserType == ENGINEER){
+     }else if(enteredUserType == "ENGINEER"){
          OurUsers = await User.find({userType : "ENGINEER"})
      }
         
